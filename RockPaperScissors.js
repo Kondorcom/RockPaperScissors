b101f590040bf3967a9ec4a967f4c632f5de8cfe
Kondorcom <kondorcom88@gmail.com>
2022-07-04 04:57:27 +0200

    Add check winner, doesnt look good

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -15,9 +15,22 @@
 //         console.log(e);
 //      });
 
+const container = document.querySelector('#container');
 
+const content = document.createElement('div');
+content.classList.add('content');
+const containerComputer = document.querySelector('#containerComputer');
 
+const content2 = document.createElement('div');
+content.classList.add('content2');
+let numberOfPlayerWins = 0;
+let numberOfComputerWins = 0;
 
+// content.textContent = 'This is the glorious text-content!';
+
+// container.appendChild(content);
+
+// startGame();
 
 function computerPlay(){
     const RPC = ["Rock", "Paper", "Scissors"];
@@ -116,27 +129,133 @@ function game(){
     console.log('Number of player wins:' + numberOfPlayerWins);
     console.log('Number of computer wins:' + numberOfComputerWins);
 }
+// function firstToFiveWins(){
 
-    let allButtons = document.querySelectorAll('button[class^=btn]');
-    console.log("Found", allButtons.length, "div which class starts with “button”.");
-    let computerInput = computerPlay();
+//     let numberOfPlayerWins = 0;
+//     let numberOfComputerWins = 0;
+
+    
+//     let allButtons = document.querySelectorAll('button[class^=btn]');
+//     console.log("Found", allButtons.length, "div which class starts with “button”.");
+    
+//     while(numberOfComputerWins < 5 || numberOfPlayerWins < 5){
+//         if (document.querySelectorAll('button[class^=btn]').clicked == true){
+//             console.log("You clicked:", this.innerHTML);
+    
+//     let computerInput = computerPlay();
+//     for (let i = 0; i < allButtons.length; i++) {
+//         allButtons[i].addEventListener('click', function() {
+//         console.clear();
+//         // console.log("You clicked:", this.innerHTML);
+//         // let playerInput = this.innerHTML;
+//         console.log('Computer input: ' + computerInput);
+//         // if (document.querySelectorAll('button[class^=btn]').clicked == true){
+//         //     let checkWins = checkWinnerOfRound(playRound(playerInput,computerInput));
+//         // if (checkWins === 0){
+//         //     numberOfPlayerWins++;
+//         // }   else if (checkWins === 2){
+//         //     numberOfComputerWins ++;
+//         // }
+//         // }
+        
+//         console.log('Player input: ' + playerInput);
+//       });
+//       if (document.querySelectorAll('button[class^=btn]').clicked == true){
+//         console.log("You clicked:", this.innerHTML);
+//         let playerInput = this.innerHTML;
+//             let checkWins = checkWinnerOfRound(playRound(playerInput,computerInput));
+//         if (checkWins === 0){
+//             numberOfPlayerWins++;
+//         }   else if (checkWins === 2){
+//             numberOfComputerWins ++;
+//         }
+//         }
+//     }
+//       content.textContent = numberOfPlayerWins;
+
+//       container.appendChild(content);
+//     }  
+    
+// }
+
+
+// checkWinnerOfGame(numberOfPlayerWins, numberOfComputerWins);
+//     console.log('Number of player wins:' + numberOfPlayerWins);
+//     console.log('Number of computer wins:' + numberOfComputerWins);
+// }
+function startGame(playerInputStart){
+
+    console.log("You clicked:", playerInputStart);
+    console.log("Start game:");
+    if (playerInputStart === undefined){
+
+    
+let allButtons = document.querySelectorAll('button[class^=btn]');
+    console.log("Found", allButtons.length, "div which class starts with “btn”.");
     for (let i = 0; i < allButtons.length; i++) {
         allButtons[i].addEventListener('click', function() {
         console.clear();
         console.log("You clicked:", this.innerHTML);
         let playerInput = this.innerHTML;
-        console.log('Computer input: ' + computerInput);
-        if (document.querySelectorAll('button[class^=btn]').clicked == true){
-            playRound(playerInput,computerInput);
-        }
+       
+            
         
-        console.log('Player input: ' + playerInput);
-      });
-     
-      
-      
-      
+    
+    
+        });
+
+
+        
     }
+    if (playerInput === undefined){
+
+        console.log("Player input undefined:", playerInputStart);
+        firstToFiveWins(playerInput);
+
+    }
+
+}   else{
+        console.log("Player input defined:", playerInputStart);
+        firstToFiveWins(playerInputStart);
+    }
+
+    // if (document.querySelectorAll('button[class^=btn]').clicked == true){
+    //     console.log("first to five clicked :", this.innerHTML);
+        
+    //     firstToFiveWins(playerInput);
+    // }
+}
+
+
+function firstToFiveWins(playerInputClick){
+    
+    
+
+    console.log('Player input click from start game: ' + playerInputClick);
+        let computerInput = computerPlay();
+        console.log('Computer input: ' + computerInput);
+        let checkWins = checkWinnerOfRound(playRound(playerInputClick,computerInput));
+        if (checkWins === 0){
+            numberOfPlayerWins++;
+        }   else if (checkWins === 2){
+            numberOfComputerWins ++;
+        }
+
+    
+        
+        
+        content.textContent = numberOfPlayerWins;
+        container.appendChild(content);
+        content2.textContent = numberOfComputerWins;
+        container.appendChild(content2);
+        console.log('number of player wins: ',numberOfPlayerWins )
+        console.log('number of computer wins: ',numberOfComputerWins )
+        checkWinner(numberOfPlayerWins,numberOfComputerWins);
+    //   if (numberOfComputerWins < 5 || numberOfPlayerWins < 5){
+    //     startGame();
+    //   }
+    }
+
 
 
 function buttonClickFunction(){
@@ -160,6 +279,20 @@ function checkWinnerOfGame(numberOfPlayerWins, numberOfComputerWins){
     }   else{
         alert('Its a tie');
     }
+}
+function checkWinner(numberOfPlayerWins,numberOfComputerWins){
+    if (numberOfPlayerWins === 5){
+        alert('You won');
+       restart();
+        
+    }else if (numberOfComputerWins === 5){
+        alert('Computer won');
+        restart();
+    }
+}
+function restart(){
+    numberOfComputerWins = 0;
+    numberOfPlayerWins = 0;
 }
 
 function checkWinnerOfRound(playRound){
